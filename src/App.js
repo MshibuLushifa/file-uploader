9d8039875b280ab13bc08fccbbdd7af9d5753547
Lushifa <48402015+MshibuLushifa@users.noreply.github.com>
2021-03-06 19:57:28 +0530

    Generating image url and display to img element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            file: null
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            file: URL.createObjectURL(event.target.files[0])
+        })
+    }
+
+    render() {
+
+        console.log(this.state.file);
+
+        return (
+            <div className="App">
+                <div>
+                    {
+                        this.state.file === null
+                            ?
+                            <svg className="image-container"
+                                fill="currentColor"
+                                viewBox="0 0 24 24">
+                                <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                            </svg>
+                            :
+                            <img className="image-container" src={this.state.file} alt="Uploading file" />
+                    }
+                </div>
+                <div>
+                    {/* <button onClick = { ()=> this.handleChange() }>
+                        Upload file
+                    </button> */}
+                    {/* <input type="file" onChange={this.handleChange} /> */}
+                    <label htmlFor="file-upload" className="custom-file-upload">
+                        Custom Upload
+                    </label>
+                    <input id="file-upload" type="file" onChange={this.handleChange}/>
+                </div>
+            </div>
+        );
+    }
+}
